575204142a53c36ba891db00a1de1b5498509987
Jason Pilz <jasonpilz@gmail.com>
2016-04-14 10:38:28 -0600

    add and implement test file mariobot_2_test.rb

---
+++ bot_saves_princess_2/test/mariobot_2_test.rb
@@ -0,0 +1,77 @@
+require_relative '../lib/mariobot_2.rb'
+require 'minitest/autorun'
+require 'stringio'
+
+module Kernel
+  def capture_stdout
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out
+  ensure
+    $stdout = STDOUT
+  end
+end
+
+class Mariobot2Test < Minitest::Test
+  def test_can_output_next_move_for_peach_above_mario
+    skip
+    out = capture_stdout do
+      mariobot = Mariobot2.new('3',
+                               '1 1',
+                               ["-p-",
+                                "-m-",
+                                "---"])
+      mariobot.next_move
+    end
+
+    assert_equal "UP\n", out.string
+  end
+
+  def test_can_output_next_move_for_peach_below_mario
+    out = capture_stdout do
+      mariobot = Mariobot2.new('5',
+                               '2 1',
+                               ["-----",
+                                "-----",
+                                "-m---",
+                                "-----",
+                                "p----"])
+      mariobot.next_move
+    end
+
+    assert_equal "DOWN\n", out.string
+  end
+
+  def test_can_output_next_move_for_peach_left_of_mario
+    skip
+    out = capture_stdout do
+      mariobot = Mariobot2.new('5',
+                               '2 3',
+                               ["-----",
+                                "-----",
+                                "p--m-",
+                                "-----",
+                                "-----"])
+      mariobot.next_move
+    end
+
+    assert_equal "LEFT\n", out.string
+  end
+
+  def test_can_output_next_move_for_peach_right_of_mario
+    skip
+    out = capture_stdout do
+      mariobot = Mariobot2.new('5',
+                               '2 1',
+                               ["-----",
+                                "-----",
+                                "-m--p",
+                                "-----",
+                                "-----"])
+      mariobot.next_move
+    end
+
+    assert_equal "RIGHT\n", out.string
+  end
+end
